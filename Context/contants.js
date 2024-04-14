9247154639f1b994403075c518dc3e94ef39c0df
zaid <developerzaid@gmail.com>
2024-04-14 18:03:37 +0100

    first commit

---
+++ Context/contants.js
@@ -0,0 +1,8 @@
+import crowdFunding from "./CrowdFunding.json";
+
+//NFT MARKETPLACE
+//LOCAL HOST TESTING HARDHAT
+// export const CrowdFundingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+// sepolia ETH  testnet
+export const CrowdFundingAddress = "0x802d2E8D60535A3A3cF25A0D0ca386Eb72C566D9";
+export const CrowdFundingABI = crowdFunding.abi;
